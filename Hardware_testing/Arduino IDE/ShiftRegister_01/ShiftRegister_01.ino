a03e41fa5926b228f9c747635b8122394d495e61
Madhushanka00 <e19226@eng.pdn.ac.lk>
2023-12-06 10:50:52 +0530

    Added shift register testing files
    Here I used 2 of 72HC595 8 bit shift registers. The folder includes sketch file (.ino) and circuit diagram (.png) and the schematic view of the circuit (.pdf).
    Thinking about connect 32 registers in series for control 16x16 single led layer.

---
+++ Hardware_testing/Arduino IDE/ShiftRegister_01/ShiftRegister_01.ino
@@ -0,0 +1,30 @@
+
+// This is a simple example of how to use the shift register
+// and here I used only one shift register
+int clearPin = 5;
+int serialData = 6;
+int clockPin = 7;
+int latchPin = 8;
+
+void setup()
+{
+
+    pinMode(clearPin, OUTPUT);
+    pinMode(serialData, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(latchPin, OUTPUT);
+
+    digitalWrite(clearPin, LOW);  // pin is ative-low, this clerars the shift register
+    digitalWrite(clearPin, HIGH); // Pin is inactive
+}
+
+void loop()
+{
+    for (int i = 0; i < 512; i++)
+    {
+        digitalWrite(latchPin, LOW); // Pull latch LOW to start sending data
+        shiftOut(serialData, clockPin, MSBFIRST, i);
+        digitalWrite(latchPin, HIGH); // Pull latch HIGH to stop sending data
+        delay(50);
+    }
+}
