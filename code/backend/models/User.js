fe7c25a422bcc30095c4e34c15043b6efbafc164
spmwitharana <e19505@eng.pdn.ac.lk>
2024-01-05 12:04:52 +0530

    backend security part update

--- code/backend/models/User.js
+++ code/backend/models/User.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 
 
 const UserSchema = new mongoose.Schema({
-    username: {
+    /*username: {
         type: String,
         required: true,
         unique: true,
-    },
-    password: {
+    },*/
+
+    /*password: {
         type: String,
         required: true,
     },
@@ -17,5 +18,44 @@ const UserSchema = new mongoose.Schema({
         unique: true,
     }
 })
+
+module.exports = mongoose.model("User", UserSchema);*/
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Pre-save middleware to hash the password before saving the user
+UserSchema.pre("save", async function (next) {
+  try {
+    // Only hash the password if it has been modified or is new
+    if (!this.isModified("password")) {
+      return next();
+    }
+
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash the password with the generated salt
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+
+    // Update the user's password with the hashed password
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 module.exports = mongoose.model("User", UserSchema);
