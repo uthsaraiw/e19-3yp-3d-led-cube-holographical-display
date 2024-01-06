b1da8295c13025bdaab0cdbf2d25f8934cb8cb21
Bambara123 <e19034@eng.pdn.ac.lk>
2024-01-06 10:34:19 +0530

    now you can login and register using UI.

--- code/backend/index.js
+++ code/backend/index.js
@@ -5,21 +5,20 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const cors = require("cors");
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(console.log("Connected to MongoDB"))
-    .catch(err => console.log(err));
-
-
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 // middleware for json()
 app.use(express.json());
 //sd
-
-
+app.use(cors());
 // user routes
 app.use("/api/", require("./routes/user"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-})
+  console.log(`Server is running on PORT ${PORT}`);
+});
