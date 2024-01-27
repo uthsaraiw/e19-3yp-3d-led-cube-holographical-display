138c29f0b492cf67ef8498221e30140fa904a145
spmwitharana <e19505@eng.pdn.ac.lk>
2024-01-28 00:42:23 +0530

    put caption and date in post

--- code/backend/models/postFile.js
+++ code/backend/models/postFile.js
@@ -1,18 +1,8 @@
 // const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-// const fileSchema = new mongoose.Schema({
-//   email: { type: String },
-//   files: [{
-//     fileType: { type: String },
-//     fileContent: { type: Buffer, required: true },
-//   }],
-// });
-
-// const PostFile = mongoose.model('PostFile', fileSchema);
-
-// module.exports = PostFile;
-
-
+// const postFileSchema = new Schema({
+//   email: { type: String, required: true },
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -24,14 +14,26 @@ const postFileSchema = new Schema({
   object: { type: Buffer },
   reactions: {
     count: { type: Number, default: 0 },
-    users: [{ type: String }] , // Array of user emails who liked the post
+    users: [{ type: String }], // Array of user emails who liked the post
   },
-  commentsCount: { type: Number, default: 0 }, 
+  commentsCount: { type: Number, default: 0 },
   //comments: { type: [String], default: [] },
   comments: [{
     email: { type: String, required: true },
     comment: { type: String, required: true },
   }],
+  caption: { type: String },
+  createdAt: { type: String }, // Not required, set automatically using middleware
+});
+
+// Set the creation date before saving the document
+postFileSchema.pre('save', function (next) {
+  if (!this.createdAt) {
+    const currentDate = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    this.createdAt = currentDate.toLocaleDateString('en-US', options);
+  }
+  next();
 });
 
 const PostFile = mongoose.model('PostFile', postFileSchema);
