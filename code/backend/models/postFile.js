1823d2404ecdca4b85b6226d198bbecc34dc7b50
spmwitharana <e19505@eng.pdn.ac.lk>
2024-01-27 23:27:21 +0530

    update

--- code/backend/models/postFile.js
+++ code/backend/models/postFile.js
@@ -1,29 +1,16 @@
 // const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
 
-// const postFileSchema = new Schema({
-//   email: { type: String, required: true },
-//   image: { type: Buffer },
-//   video: { type: Buffer },
-//   code: { type: Buffer },
-//   object: { type: Buffer },
-//   reactions: {
-//     count: { type: Number, default: 0 },
-//     users: [{ type: String }] , // Array of user emails who liked the post
-//   },
-//   commentsCount: { type: Number, default: 0 }, 
-//   //comments: { type: [String], default: [] },
-//   comments: [{
-//     email: { type: String, required: true },
-//     comment: { type: String, required: true },
+// const fileSchema = new mongoose.Schema({
+//   email: { type: String },
+//   files: [{
+//     fileType: { type: String },
+//     fileContent: { type: Buffer, required: true },
 //   }],
-//   caption: {type: String}
 // });
 
-// const PostFile = mongoose.model('PostFile', postFileSchema);
+// const PostFile = mongoose.model('PostFile', fileSchema);
 
 // module.exports = PostFile;
-
 
 
 const mongoose = require('mongoose');
@@ -37,26 +24,14 @@ const postFileSchema = new Schema({
   object: { type: Buffer },
   reactions: {
     count: { type: Number, default: 0 },
-    users: [{ type: String }], // Array of user emails who liked the post
+    users: [{ type: String }] , // Array of user emails who liked the post
   },
-  commentsCount: { type: Number, default: 0 },
+  commentsCount: { type: Number, default: 0 }, 
   //comments: { type: [String], default: [] },
   comments: [{
     email: { type: String, required: true },
     comment: { type: String, required: true },
   }],
-  caption: { type: String },
-  createdAt: { type: String }, // Not required, set automatically using middleware
-});
-
-// Set the creation date before saving the document
-postFileSchema.pre('save', function (next) {
-  if (!this.createdAt) {
-    const currentDate = new Date();
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    this.createdAt = currentDate.toLocaleDateString('en-US', options);
-  }
-  next();
 });
 
 const PostFile = mongoose.model('PostFile', postFileSchema);
